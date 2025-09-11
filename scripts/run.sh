6b8bf4c7ebf174ce77a30c6f2fca7eb48624f54e
kmg171 <qweaiiok4781@gmail.com>
2025-09-11 17:06:13 +0900

    민규: uv 패키지 설치 및 psycopg 추가

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,9 @@
-#!/bin/sh
+﻿#!/bin/bash
 set -e
 
+# 마이그레이션 체크 및 적용
 python manage.py makemigrations --check --noinput
 python manage.py migrate
+
+# Gunicorn 실행
 gunicorn --bind 0.0.0.0:8000 config.wsgi:application --workers 2
