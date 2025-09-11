d44811001975fce965e0d26421019ab7fcbc52e2
fullrestic <restinplastic@naver.com>
2025-09-11 18:37:24 +0900

    carts url 등록

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+import apps
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/cart/", include('apps.carts.urls')),
 ]
