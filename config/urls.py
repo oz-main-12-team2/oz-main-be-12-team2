37c4fa460ca23a46ee36ea8126a6ea146afb698b
zzinypark <zzinypark21@gamil.com>
2025-09-12 16:24:45 +0900

    static 오류 수정

--- config/urls.py
+++ config/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -26,3 +28,6 @@ urlpatterns = [
     path('api/admin/user/', include('apps.users.admin_urls')),
     path('api/orders/', include('apps.orders.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
