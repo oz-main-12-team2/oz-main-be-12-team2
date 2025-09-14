946501464ffd2faf29a0a86cfe94023a6fb32854
fullrestic <restinplastic@naver.com>
2025-09-14 15:50:25 +0900

    users admin url 잘못 수정한 부분 복구

--- apps/users/admin_urls.py
+++ apps/users/admin_urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .admin_views import AdminPaymentListView, AdminPaymentDetailView
+
+from . import admin_views
 
 urlpatterns = [
-    path("payments/", AdminPaymentListView.as_view(), name="admin-payment-list"),
-    path("payments/<int:pk>/", AdminPaymentDetailView.as_view(), name="admin-payment-detail"),
+    path('users/', admin_views.admin_user_list, name='admin_user_list'),
+    path('user/<int:user_id>/', admin_views.admin_user_detail, name='admin_user_detail'),
+    path('user/<int:user_id>/', admin_views.admin_user_update, name='admin_user_update'),
+    path('user/<int:user_id>/', admin_views.admin_user_delete, name='admin_user_delete'),
 ]
