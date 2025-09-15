aba0c80106b9b19c659ce4e96c53165563d89712
fullrestic <restinplastic@naver.com>
2025-09-15 15:35:41 +0900

    payments test 일부 수정

--- apps/payments/serializers.py
+++ apps/payments/serializers.py
@@ -1,24 +1,17 @@
 from rest_framework import serializers
 
+from ..orders.models import Order
 from .models import Payment
 
 
 class PaymentSerializer(serializers.ModelSerializer):
-    order_id = serializers.IntegerField(write_only=True)
+    # 요청과 응답 모두 order_id를 사용
+    order_id = serializers.PrimaryKeyRelatedField(
+        queryset=Order.objects.all(),
+        source="order"  # 내부적으로 Payment.order(FK)와 연결
+    )
 
     class Meta:
         model = Payment
         fields = ["id", "order_id", "method", "total_price", "status", "created_at"]
         read_only_fields = ["id", "created_at"]
-
-    def create(self, validated_data):
-        order_id = validated_data.pop("order_id")
-        from apps.orders.models import Order  # 지연 import
-
-        try:
-            order = Order.objects.get(id=order_id)
-        except Order.DoesNotExist:
-            raise serializers.ValidationError({"order_id": "해당 주문을 찾을 수 없습니다."})
-
-        payment = Payment.objects.create(order=order, **validated_data)
-        return payment
