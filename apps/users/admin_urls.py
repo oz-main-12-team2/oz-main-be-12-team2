6fa0f702ae38a2de04d55fae4285d943a5151b5d
zzinypark <zzinypark21@gamil.com>
2025-09-15 18:23:05 +0900

    admin/user/ api수정

--- apps/users/admin_urls.py
+++ apps/users/admin_urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import admin_views
 
 urlpatterns = [
-    path("users/", admin_views.admin_user_list, name="admin_user_list"),
-    path("user/<int:user_id>/", admin_views.AdminUserDetailView.as_view(), name="admin_user_detail"),
+    path("", admin_views.admin_user_list, name="admin_user_list"),
+    path("<int:user_id>/", admin_views.AdminUserDetailView.as_view(), name="admin_user_detail"),
 ]
