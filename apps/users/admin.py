addddbcf435f3ac01c211bae8b9ff17844567ebf
fullrestic <restinplastic@naver.com>
2025-09-22 13:02:59 +0900

    test script 적용

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 from .models import User
 
+
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ("id", "__str__")  # __str__ 은 모델에 정의된 대표 필드
-
