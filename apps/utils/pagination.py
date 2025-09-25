2ab364e12c108af6ac7c16587ae996d7d86e4aa7
fullrestic <restinplastic@naver.com>
2025-09-25 16:45:19 +0900

    ProductPagination -> CustomPagination 수정 후 관리자 기능에 적용

--- apps/utils/pagination.py
+++ apps/utils/pagination.py
@@ -1,5 +1,5 @@
 from rest_framework.pagination import PageNumberPagination
 
 
-class ProductPagination(PageNumberPagination):
+class CustomPagination(PageNumberPagination):
     page_size_query_param = "page_size"  # 사용자가 ?page_size=20 지정 가능
