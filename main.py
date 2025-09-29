e775f7ea6271e5c53876d5b55b6643d915d70e54
fullrestic <restinplastic@naver.com>
2025-09-29 13:44:41 +0900

    불필요한 코드 및 파일 삭제

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from oz-main-be-12-team2!")
-
-
-if __name__ == "__main__":
-    main()
--- scripts/run.sh
+++ scripts/run.sh
@@ -10,15 +10,17 @@ python manage.py migrate --noinput
 echo "==> 정적 파일 수집..."
 python manage.py collectstatic --noinput
 
-echo "==> Site 객체 확인/생성..."
-python manage.py shell -c "
-from django.contrib.sites.models import Site
-Site.objects.get_or_create(
-    id=1,
-    defaults={'domain': 'localhost:8000', 'name': 'localhost'}
-)
-print('Site 객체 생성 완료')
-"
+# 필요 없음
+
+#echo "==> Site 객체 확인/생성..."
+#python manage.py shell -c "
+#from django.contrib.sites.models import Site
+#Site.objects.get_or_create(
+#    id=1,
+#    defaults={'domain': 'localhost:8000', 'name': 'localhost'}
+#)
+#print('Site 객체 생성 완료')
+#"
 
 echo "==> Gunicorn 서버 시작..."
 exec gunicorn --bind 0.0.0.0:8000 config.wsgi:application --workers 2
