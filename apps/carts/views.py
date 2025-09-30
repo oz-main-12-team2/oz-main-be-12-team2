ae339609a3500c7cbedb499ed13f5d0ba350255d
fullrestic <restinplastic@naver.com>
2025-09-30 15:17:39 +0900

    장바구니 API 응답 형식 수정

--- apps/carts/views.py
+++ apps/carts/views.py
@@ -1,5 +1,7 @@
 from django.db.models import Prefetch
+from django.http import Http404
 from rest_framework import generics, permissions, serializers, status
+from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -55,6 +57,11 @@ class CartProductUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
     def get_queryset(self):
         return CartProduct.objects.filter(cart__user=self.request.user)
 
+    def get_serializer_class(self):
+        if self.request.method == "PUT":
+            return CartProductUpdateSerializer  # ✅ 수량만
+        return CartProductSerializer  # ✅ 삭제 시 응답에는 전체 정보
+
     # PUT 요청 처리 (수량 변경)
     def update(self, request, *args, **kwargs):
         partial = kwargs.pop("partial", False)
@@ -77,13 +84,15 @@ class CartProductUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
         self.perform_destroy(instance)
         return Response(
             {"detail": "상품이 장바구니에서 삭제되었습니다."},
-            status=status.HTTP_204_NO_CONTENT,
+            status=status.HTTP_200_OK
         )
 
-    def get_serializer_class(self):
-        if self.request.method == "PUT":
-            return CartProductUpdateSerializer  # ✅ 수량만
-        return CartProductSerializer  # ✅ 삭제 시 응답에는 전체 정보
+    # 404 에러 메세지 커스텀
+    def get_object(self):
+        try:
+            return super().get_object()
+        except Http404:
+            raise NotFound(detail="장바구니에 해당 상품이 없습니다.")
 
 
 # ✅ 장바구니 전체 비우기
@@ -91,12 +100,18 @@ class CartClearView(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def delete(self, request):
-        try:
-            cart = Cart.objects.get(user=request.user)
-            cart.items.all().delete()  # 해당 장바구니에 담긴 상품 전체 삭제
-            return Response({"detail": "장바구니가 비워졌습니다."}, status=status.HTTP_204_NO_CONTENT)
-        except Cart.DoesNotExist:
+        cart = Cart.objects.get(user=request.user)
+
+        # 장바구니가 이미 비어있는 경우
+        if not cart.items.exists():
             return Response(
-                {"detail": "장바구니가 존재하지 않습니다."},
-                status=status.HTTP_404_NOT_FOUND,
+                {"detail": "장바구니가 이미 비어있습니다."},
+                status=status.HTTP_400_BAD_REQUEST,
             )
+
+        # 장바구니 비우기
+        cart.items.all().delete()
+        return Response(
+            {"detail": "장바구니가 비워졌습니다."},
+            status=status.HTTP_200_OK,
+        )
