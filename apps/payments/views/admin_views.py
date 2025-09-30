c353166bb3cc0199e0c940f5b8c3a3ee93cacb05
fullrestic <restinplastic@naver.com>
2025-09-30 18:59:07 +0900

    관리자 주문내역 조회 Serializer 추가 - 사용자 정보 포함

--- apps/payments/views/admin_views.py
+++ apps/payments/views/admin_views.py
@@ -6,13 +6,13 @@ from rest_framework import generics, permissions
 from apps.core.pagination import CustomPagination
 from apps.payments.filters import PaymentFilter
 from apps.payments.models import Payment
-from apps.payments.serializers import PaymentSerializer
+from apps.payments.serializers import AdminPaymentSerializer
 
 
 # ✅ 전체 결제 내역 조회 (관리자 전용)
 class AdminPaymentListView(generics.ListAPIView):
     queryset = Payment.objects.all().order_by("-created_at")
-    serializer_class = PaymentSerializer
+    serializer_class = AdminPaymentSerializer
     permission_classes = [permissions.IsAdminUser]
     pagination_class = CustomPagination
 
@@ -53,5 +53,5 @@ class AdminPaymentListView(generics.ListAPIView):
 # ✅ 특정 결제 내역 상세 조회 (관리자 전용)
 class AdminPaymentDetailView(generics.RetrieveAPIView):
     queryset = Payment.objects.all()
-    serializer_class = PaymentSerializer
+    serializer_class = AdminPaymentSerializer
     permission_classes = [permissions.IsAdminUser]
